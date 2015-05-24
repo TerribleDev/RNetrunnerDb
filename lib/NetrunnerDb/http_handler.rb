52675e178f345ec44ecd74b86ab90c97c347042b
Tommy Parnell <tparnell8@gmail.com>
2015-05-24 10:57:17 -0400

    fix minor bugs

--- lib/NetrunnerDb/http_handler.rb
+++ lib/NetrunnerDb/http_handler.rb
@@ -6,7 +6,7 @@ module NetrunnerDb
 
     def self.process_request(url_param)
       response = Faraday.get("http://netrunnerdb.com#{url_param}")
-      nil unless response.env['status'] == 200
+      return nil unless response.env['status'] == 200
       JSON.parse(response.body)
     end
 
@@ -17,13 +17,13 @@ module NetrunnerDb
       return HttpHandler.process_request('/api/cards/')
     end
     def self.get_set(set_code)
-      return HttpHandler.process_request("/api/set/#{set_code}/")
+      return HttpHandler.process_request("/api/set/#{set_code}")
     end
     def self.get_card(card_code)
-      return HttpHandler.process_request("/api/card/#{card_code}/")
+      return HttpHandler.process_request("/api/card/#{card_code}")
     end
     def self.get_decklist(decklist_id)
-      return HttpHandler.process_request("/api/decklist/#{decklist_id}/")
+      return HttpHandler.process_request("/api/decklist/#{decklist_id}")
     end
 
   end
